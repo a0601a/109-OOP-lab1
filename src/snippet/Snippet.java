3e0c503de8e53a40d37484dc183606eb61b346a1
user <user@EC06>
2020-09-29 09:36:34 +0800

    test

---
+++ src/snippet/Snippet.java
@@ -0,0 +1,10 @@
+package snippet;
+
+public class Snippet {
+	public static void main(String[] args) {
+		System.out.println("I am ¬I¹©²±");
+		System.out.println("My NID is D0543431");
+		System.out.println("This is Lab 1 HW");
+	}
+}
+
